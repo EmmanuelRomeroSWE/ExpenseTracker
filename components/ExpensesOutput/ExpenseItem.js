1fc6ca8241bb6ff72b8a2256eb898a84c81e1001
Emmanuel <romero512497@gmail.com>
2022-11-25 10:27:56 -0600

    add modal for editing and adding expenses, create a dynamic IconButton component, and adjust date formating in list

--- components/ExpensesOutput/ExpenseItem.js
+++ components/ExpensesOutput/ExpenseItem.js
@@ -1,15 +1,24 @@
-import { Pressable, StyleSheet, Text, View, ViewComponent } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import { Pressable, StyleSheet, Text, View} from 'react-native'
 import { GlobalStyles } from '../../constants/styles'
+import { getFormattedDate } from '../../util/date'
 
-function ExpenseItem({ description, amount, date }){
-    return <Pressable>
+function ExpenseItem({ id, description, amount, date }){
+    const navigation = useNavigation()
+
+    function expensePressHandler(){
+        navigation.navigate('ManageExpense', {
+            expenseId: id
+        })
+    }
+    return <Pressable onPress={expensePressHandler} style={({pressed}) => pressed && styles.pressed}>
         <View style={styles.expenseItem}>
             <View>
-            <Text>{description}</Text>
-            <Text>{amount}</Text>
+            <Text style={[styles.textBase, styles.description]}>{description}</Text>
+            <Text style={styles.textBase}>{getFormattedDate(date)}</Text>
             </View>
-            <View>
-            <Text>{date}</Text>
+            <View style={styles.amountContainer}>
+            <Text style={styles.amount}>{amount.toFixed(2)}</Text>
             </View>
         </View>
     </Pressable>
@@ -18,6 +27,9 @@ function ExpenseItem({ description, amount, date }){
 export default ExpenseItem
 
 const styles = StyleSheet.create({
+    pressed: {
+        opacity: 0.75
+    },
     expenseItem: {
         padding: 12,
         marginVertical: 8,
@@ -27,5 +39,29 @@ const styles = StyleSheet.create({
         borderRadius: 6,
         elevation: 3,
         shadowColor: GlobalStyles.colors.gray500,
+        shadowRadius: 4,
+        shadowOffset: {width: 1, height: 1},
+        shadowOpacity: 0.4
+    },
+    textBase: {
+        color: GlobalStyles.colors.primary50
+    },
+    description: {
+        fontSize: 16,
+        marginBottom: 4,
+        fontWeight: 'bold'
+    },
+    amountContainer: {
+        paddingHorizontal: 12,
+        paddingVertical: 4,
+        backgroundColor: "white", 
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 4,
+        minWidth: 100
+    },
+    amount: {
+        color: GlobalStyles.colors.primary500,
+        fontWeight: 'bold'
     }
 })
